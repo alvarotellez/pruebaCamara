f4bce59386ce01e921ed736854644a30353a0f38
alvarotellez <alvarotellezhidalgo@gmail.com>
2016-11-10 13:48:18 +0100

    Initial commit

---
+++ app/src/main/java/es/iesnervion/atellez/pruebacamara/MainActivity.java
@@ -0,0 +1,58 @@
+package es.iesnervion.atellez.pruebacamara;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    Button hacerFoto;
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        hacerFoto = (Button) findViewById(R.id.btn);
+        hacerFoto.setOnClickListener(this);
+
+        img = (ImageView) findViewById(R.id.foto);
+
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            img.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+}
